73fc0b909f81662ee77f70afce993466065f9097
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-07-13 16:38:34 -0300

    Search update
    Integração da função de pesquisa

--- detalhe.php
+++ detalhe.php
@@ -1,33 +1,33 @@
 <?php require_once("../../connection/conexao.php"); ?>
-
 <?php
-    if(isset($_GET["codigo"])){
-        $produtoID = $_GET["codigo"];
+    if ( isset($_GET["codigo"]) ) {
+        $produto_id = $_GET["codigo"];
     } else {
         Header("Location: listagem.php");
     }
 
+    // Consulta ao banco de dados
     $consulta = "SELECT * ";
-    $consulta .= " FROM produtos ";
-    $consulta .= " WHERE produtoID = {$produtoID}";
-    $detalhe = mysqli_query($conecta,$consulta);
+    $consulta .= "FROM produtos ";
+    $consulta .= "WHERE produtoID = {$produto_id} ";
+    $detalhe    = mysqli_query($conecta,$consulta);
 
-    if(!$detalhe){
+    // Testar erro
+    if ( !$detalhe ) {
         die("Falha no Banco de dados");
     } else {
         $dados_detalhe = mysqli_fetch_assoc($detalhe);
-
-        $nomeproduto = $dados_detalhe["nomeproduto"];
-        $descriçao = $dados_detalhe["descricao"];
-        $codigobarra = $dados_detalhe["codigobarra"];
-        $tempoentrega = $dados_detalhe["tempoentrega"];
-        $precorevenda = $dados_detalhe["precorevenda"];
-        $precounitario = $dados_detalhe["precounitario"];
-        $estoque = $dados_detalhe["estoque"];
-        $imagemgrande = $dados_detalhe["imagemgrande"];
+        $produtoID      = $dados_detalhe["produtoID"];
+        $nomeproduto    = $dados_detalhe["nomeproduto"];
+        $descricao      = $dados_detalhe["descricao"];
+        $codigobarra    = $dados_detalhe["codigobarra"];
+        $tempoentrega   = $dados_detalhe["tempoentrega"];
+        $precorevenda   = $dados_detalhe["precorevenda"];
+        $precounitario  = $dados_detalhe["precounitario"];
+        $estoque        = $dados_detalhe["estoque"];
+        $imagemgrande   = $dados_detalhe["imagemgrande"];
     }
 ?>
-
 <!doctype html>
 <html>
     <head>
@@ -42,20 +42,21 @@
 
     <body>
         <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?> 
+        <?php include_once("../_incluir/funcoes.php"); ?>
         
         <main>  
             <div id="detalhe_produto">
                 <ul>
-                    <li class="imagem"><img src="<?php echo $imagemgrande ?>" alt=""></li>
+                    <li class="imagem"><img src="<?php echo $imagemgrande ?>"></li>
                     <li><h2><?php echo $nomeproduto ?></h2></li>
-                    <li><?php echo $descriçao ?></li>
-                    <li>Código de Barra: <?php echo $codigobarra ?></li>
-                    <li>Tempo de entrega: <?php echo $tempoentrega ?></li>
-                    <li>Preço revenda: <?php echo real_format($precorevenda) ?></li>
-                    <li>Preço unitário: <?php echo real_format($precounitario) ?></li>
-                    <li>Estoque: <?php echo $estoque ?></li>
+                    <li><b>Descrição: </b><?php echo $descricao ?></li>
+                    <li><b>Código de Barra: </b><?php echo $codigobarra ?></li>
+                    <li><b>Tempo de Entrega: </b><?php echo $tempoentrega ?></li>
+                    <li><b>Preço Revenda: </b><?php echo real_format($precorevenda) ?></li>
+                    <li><b>Preço Unitário: </b><?php echo real_format($precounitario) ?></li>
+                    <li><b>Estoque: </b><?php echo $estoque ?></li>
                 </ul>
+               
             </div>
         </main>
 
--- listagem.php
+++ listagem.php
@@ -6,7 +6,12 @@
 
     // Consulta ao banco de dados
     $produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena ";
-    $produtos .= "FROM produtos ";
+    $produtos .= " FROM produtos ";
+    
+    if(isset($_GET["produto"])){
+        $nome_produto = $_GET["produto"];
+        $produtos .= " WHERE nomeproduto LIKE '%{$nome_produto}%' ";
+    }
     $resultado = mysqli_query($conecta, $produtos);
     if(!$resultado) {
         die("Falha na consulta ao banco");   
@@ -16,28 +21,34 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Curso PHP Integração com MySQL</title>
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
         <link href="_css/produtos.css" rel="stylesheet">
+        <link href="_css/produto_pesquisa.css" rel="stylesheet">
     </head>
 
     <body>
         <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?>  
-              
-        <main>  
-            
+        <?php include_once("../_incluir/funcoes.php"); ?>
+        
+        <main>        
+            <div id="janela_pesquisa">
+                <form action="listagem.php" method="get">
+                    <input type="text" name="produto" placeholder="Nome do produto">
+                    <input type="image" name="pesquisa" src="../_assets/botao_search.png">
+                </form>
+            </div>
+
            <div id="listagem_produtos"> 
             <?php
                 while($linha = mysqli_fetch_assoc($resultado)) {
             ?>
                 <ul>
                     <li class="imagem">
-                        <a href="detalhe.php?codigo=<?php echo $linha["produtoID"] ?> ">
-                            <img src="<?php echo  $linha["imagempequena"] ?>">
+                        <a href="detalhe.php?codigo=<?php echo $linha['produtoID'] ?>">
+                            <img src="<?php echo $linha["imagempequena"] ?>">
                         </a>
                     </li>
                     <li><h3><?php echo $linha["nomeproduto"] ?></h3></li>
