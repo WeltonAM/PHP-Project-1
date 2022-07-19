aa2bf31fe0ee67594dbe68e3d398102cf8a2ed76
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-07-19 15:56:20 -0300

    Update the shipping information

---
+++ alteracao.php
@@ -0,0 +1,134 @@
+<?php require_once("../../connection/conexao.php"); ?>
+
+<?php 
+    if(isset($_POST["cidade"])) {
+        $nome = $_POST["nometransportadora"];
+        $endereco = $_POST["endereco"];
+        $cidade = $_POST["cidade"];
+        $estado = $_POST["estados"];
+        $cep = $_POST["cep"];
+        $cnpj = $_POST["cnpj"];
+        $telefone = $_POST["telefone"];
+        $tid = $_POST["transportadoraID"];
+
+        $alterar = "UPDATE transportadoras ";
+        $alterar .= " SET ";
+        $alterar .= " nometransportadora = '{$nome}', ";
+        $alterar .= " endereco = '{$endereco}', ";
+        $alterar .= " cidade = '{$cidade}', ";
+        $alterar .= " estado = {$estado}, ";
+        $alterar .= " cep = '{$cep}', ";
+        $alterar .= " cnpj = '{$cnpj}', ";
+        $alterar .= " telefone = '{$telefone}' ";
+        $alterar .= " WHERE transportadoraID = {$tid} ";
+
+        $operacao_alteracao = mysqli_query($conecta,$alterar);
+        if(!$operacao_alteracao) {
+            die("Erro no banco de dados");
+        } else {
+            header("location:listagem.php");
+        }
+    }
+
+
+    $transp = "SELECT * FROM transportadoras ";
+    
+    if(isset($_GET["codigo"])) {
+        $id = $_GET["codigo"];
+        $transp .= " WHERE transportadoraID = {$id}";
+    } else {
+        $transp .= " WHERE transportadoraID = 1";
+    }
+
+    $con_transp = mysqli_query($conecta, $transp);
+    if(!$con_transp) {
+        die("Erro no banco de dados");
+    } else {
+        $info_transp = mysqli_fetch_assoc($con_transp);
+    }
+
+    // consulta da tabela dos dados
+    $estados = "SELECT estadoID, nome FROM estados ";
+    $lista_estados = mysqli_query($conecta,$estados);
+    if(!$lista_estados) {
+        die("Falha no banco");
+    }
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP Integração com MySQL</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/alteracao.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include_once("../_incluir/topo.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?> 
+        
+        <main>  
+            <div id="janela_formulario">
+                <form action="alteracao.php" method="post">
+                    <h2>Alteração de Transportadora</h2>
+
+                    <label for="nometransportadora">Nome da Transportadora</label>
+                    <input type="text" value="<?php echo $info_transp["nometransportadora"] ?>" name="nometransportadora">
+                    
+                    <label for="endereco">Endereço</label>
+                    <input type="text" value="<?php echo $info_transp["endereco"] ?>" name="endereco">
+
+                    <label for="cidade">Cidade</label>
+                    <input type="text" value="<?php echo $info_transp["cidade"] ?>" name="cidade">
+                    
+                    <label for="estados">Estados</label>
+                    <select name="estados" id="estado">
+                        <?php 
+                            $meuestado = $info_transp["estadoID"];
+                            while($linha = mysqli_fetch_assoc($lista_estados)) {
+                                $estado_momento = $linha["estadoID"];
+                                if($meuestado == $estado_momento) {
+
+                        ?>
+                        <option value="<?php echo $linha["estadoID"]; ?>" selected>
+                            <?php echo $linha["nome"] ?>
+                        </option>
+                        
+                        <?php 
+                            } else {
+
+                        ?>
+                        
+                        <option value="<?php echo $linha["estadoID"]; ?>">
+                            <?php echo $linha["nome"] ?>
+                        </option>
+                        <?php 
+                                }
+                            }
+                        ?>
+                    </select>
+                    
+                    <label for="cep">CEP</label>
+                    <input type="text" value="<?php echo $info_transp["cep"] ?>" name="cep">
+                    
+                    <label for="cnpj">CNPJ</label>
+                    <input type="text" value="<?php echo $info_transp["cnpj"] ?>" name="cnpj">
+
+                    <input type="hidden" name="transportadoraID" value="">
+
+                    <input type="submit" value="Confirmar Alteração">
+                </form>
+            </div>
+        </main>
+
+        <?php include_once("../_incluir/rodape.php"); ?>  
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($conecta);
+?>
--- listagem.php
+++ listagem.php
@@ -1,27 +1,13 @@
 <?php require_once("../../connection/conexao.php"); ?>
-
 <?php
-    session_start();
-
-    if(!isset($_SESSION["user_portal"])) {
-        header("location: login.php");
-    }
-
-    // Determinar localidade BR
-    setlocale(LC_ALL, 'pt_BR');
-
-    // Consulta ao banco de dados
-    $produtos = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena ";
-    $produtos .= "FROM produtos ";
-    if ( isset($_GET["produto"]) ) {
-        $nome_produto = $_GET["produto"];
-        $produtos .= "WHERE nomeproduto LIKE '%{$nome_produto}%' ";
-    }
-    $resultado = mysqli_query($conecta, $produtos);
-    if(!$resultado) {
-        die("Falha na consulta ao banco");   
+    // tabela de transportadoras
+    $tr = "SELECT * FROM transportadoras ";
+    $consulta_tr = mysqli_query($conecta, $tr);
+    if(!$consulta_tr) {
+        die("erro no banco");
     }
 ?>
+
 <!doctype html>
 <html>
     <head>
@@ -30,41 +16,27 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
-        <link href="_css/produtos.css" rel="stylesheet">
-        <link href="_css/produto_pesquisa.css" rel="stylesheet">
+        <link href="_css/novo-transportadora.css" rel="stylesheet">
     </head>
 
     <body>
         <?php include_once("../_incluir/topo.php"); ?>
-        <?php include_once("../_incluir/funcoes.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?> 
         
-        <main>
-            <div id="janela_pesquisa">
-                <form action="listagem.php" method="get">
-                    <input type="text" name="produto" placeholder="Pesquisa">
-                    <input type="image"  src="../_assets/botao_search.png">
-                </form>
-            </div>
-            
-            <div id="listagem_produtos"> 
-            <?php
-                while($linha = mysqli_fetch_assoc($resultado)) {
-            ?>
+        <main>  
+            <div id="janela_transportadoras">
+                <?php
+                    while($linha = mysqli_fetch_assoc($consulta_tr)) {
+                ?>
                 <ul>
-                    <li class="imagem">
-                        <a href="detalhe.php?codigo=<?php echo $linha['produtoID'] ?>">
-                            <img src="<?php echo $linha["imagempequena"] ?>">
-                        </a>
-                    </li>
-                    <li><h3><?php echo $linha["nomeproduto"] ?></h3></li>
-                    <li>Tempo de Entrega : <?php echo $linha["tempoentrega"] ?></li>
-                    <li>Preço unitário : <?php echo real_format($linha["precounitario"]) ?></li>
+                    <li><?php echo $linha["nometransportadora"] ?></li>
+                    <li><?php echo $linha["cidade"] ?></li>
+                    <li><a href="alteracao.php?codigo=<?php echo $linha["transportadoraID"] ?>">Alterar</a> </li>
                 </ul>
-             <?php
-                }
-            ?>           
+                <?php
+                    }
+                ?>
             </div>
-            
         </main>
 
         <?php include_once("../_incluir/rodape.php"); ?>  
